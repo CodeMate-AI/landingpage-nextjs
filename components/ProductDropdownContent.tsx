e95569c58a0a268dc10a9c797ebac168bd44ca52
Vansh <vanshk605@gmail.com>
2025-06-11 10:12:25 +0530

    update linting error

--- components/ProductDropdownContent.tsx
+++ components/ProductDropdownContent.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 import { RainbowButton } from "@/components/magicui/rainbow-button"
 import Link from 'next/link'
 
-type ProductType = "CodeMate Terminal" | "CodeMate VS Code Extension" | "CodeMate Web App" | "CodeMate for Education";
+type ProductType = "CodeMate Terminal" | "CodeMate VS Code Extension" | "CodeMate Web App" | "CodeMate for Education" | "CodeMate Agent";
 
 const products: { title: ProductType; description: string; link: string; icon: string }[] = [
     {
