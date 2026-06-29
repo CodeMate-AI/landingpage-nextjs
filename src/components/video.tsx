9cd484f8be9fcfda12587c4f9d8590e478262e5d
Biswajit Dash <biswajitdash929@gmail.com>
2026-06-29 14:57:08 +0530

    Turned off captions for the YT video

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -2,7 +2,7 @@ export default function VideoEmbed() {
   return (
     <iframe
       className="w-full h-full pointer-events-none"
-      src="https://www.youtube-nocookie.com/embed/ux5jJgNkslE?autoplay=1&mute=1&loop=1&rel=0&controls=0&modestbranding=1&showinfo=0&disablekb=1&playlist=ux5jJgNkslE"
+      src="https://www.youtube-nocookie.com/embed/ux5jJgNkslE?autoplay=1&mute=1&loop=1&rel=0&controls=0&modestbranding=1&showinfo=0&disablekb=1&playlist=ux5jJgNkslE&cc_load_policy=0"
       allow="autoplay; encrypted-media"
       allowFullScreen
       referrerPolicy="strict-origin-when-cross-origin"
