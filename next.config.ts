d2ceb66f686ed8c24d479b8c909c5770d8ca4257
vansh <vanshk605@gmail.com>
2026-06-29 14:18:24 +0530

    fixed issue for domain

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,35 @@ import { withNextVideo } from "next-video/process";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'assets.aceternity.com' },
-      { protocol: 'https', hostname: 'api.microlink.io' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'assests.aceternity.com' },
-      { protocol: 'https', hostname: 'backend.codemate.ai' },
+      { protocol: "https", hostname: "assets.aceternity.com" },
+      { protocol: "https", hostname: "api.microlink.io" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "assests.aceternity.com" },
+      { protocol: "https", hostname: "backend.codemate.ai" },
     ],
   },
-  allowedDevOrigins: ['127.0.0.1:3000', 'localhost:3000'],
+
+  allowedDevOrigins: ["127.0.0.1:3000", "localhost:3000"],
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default withNextVideo(nextConfig, { folder: 'video' });
+export default withNextVideo(nextConfig, { folder: "video" });
