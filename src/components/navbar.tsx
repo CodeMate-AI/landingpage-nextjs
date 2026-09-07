e76266c9a4906411b2ff6095a4aa0852fe01656f
Biswajit Dash <biswajitdash929@gmail.com>
2026-09-07 08:30:08 +0530

    updated the cta to contact us from get started.
    - removed contact us from resources menu at navbar
    
    - updated mobile navbar also

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -214,8 +214,8 @@ export default function Navbar() {
                   <a href='https://edu.codemate.ai/' target='_blank'>
                     <motion.h1 onMouseEnter={() => { setIsProducts(false); setIsOS(false); setIsResources(false) }} whileHover={{ opacity: 1 }} className='opacity-65'>Academy</motion.h1>
                   </a>
-                  <a href="https://app.codemate.ai" target='_blank'>
-                    <motion.button whileHover={{ opacity: 1, scale: 1.05 }} className={`${montserrat.className} px-2 py-1  bg-[#FFFFFF] text-black  rounded-sm font-semibold opacity-85 text-nowrap`}>Get Started</motion.button>
+                  <a href="/contact">
+                    <motion.button whileHover={{ opacity: 1, scale: 1.05 }} className={`${montserrat.className} px-2 py-1  bg-[#FFFFFF] text-black  rounded-sm font-semibold opacity-85 text-nowrap`}>Contact Us</motion.button>
                   </a>
                 </span>
 
@@ -363,14 +363,6 @@ export default function Navbar() {
                         <a href='/blog' className='w-full'>
                           <motion.div whileHover={{ opacity: 1 }} className="flex justify-between items-center opacity-80 w-full group hover:bg-white/10 rounded-lg px-3 py-2 transition-all duration-200">
                             <h1>Blogs</h1>
-                            <div className="size-[1.48rem] bg-white/25 rounded-full bg-opacity-90 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                              <motion.svg initial={{ rotate: 50, opacity: 0.7 }} xmlns="http://www.w3.org/2000/svg" width={17} height={17} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-up"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 5l0 14" /><path d="M16 9l-4 -4" /><path d="M8 9l4 -4" /></motion.svg>
-                            </div>
-                          </motion.div>
-                        </a>
-                        <a href='/contact' className='w-full'>
-                          <motion.div whileHover={{ opacity: 1 }} className="flex justify-between items-center opacity-80 w-full group hover:bg-white/10 rounded-lg px-3 py-2 transition-all duration-200">
-                            <h1>Contact Us</h1>
                             <div className="size-[1.48rem] bg-white/25 rounded-full bg-opacity-90 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                               <motion.svg initial={{ rotate: 50, opacity: 0.7 }} xmlns="http://www.w3.org/2000/svg" width={17} height={17} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-up"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 5l0 14" /><path d="M16 9l-4 -4" /><path d="M8 9l4 -4" /></motion.svg>
                             </div>
@@ -666,16 +658,6 @@ export default function Navbar() {
                               </motion.div>
                             </a>
                           </div>
-                          <div className='relative text-base md:text-[1.15rem] lg:text-base text-left overflow-hidden py-0.5 md:py-2 lg:py-0'>
-                            <a href="/contact">
-                              <motion.h1 className='z-20 opacity-90'>Contact Us</motion.h1>
-                              <motion.div whileHover={{ y: -50 }} transition={{ duration: 0.8 }} className='absolute h-full w-full  top-0 '>
-                                <motion.div initial={{ y: 50 }} className='h-full w-full rounded-t-md bg-cyan-600'>
-                                  <h1>Contact Us</h1>
-                                </motion.div>
-                              </motion.div>
-                            </a>
-                          </div>
                         </motion.div>
                       }
                     </motion.div>
@@ -684,9 +666,7 @@ export default function Navbar() {
                     <a href='https://edu.codemate.ai/' target='_blank'>
                       <motion.div whileHover={{ opacity: 0.6 }} className='flex gap-2 cursor-pointer'><h1 className='MenuText'>ACADEMY</h1><p className='text-[3vw] mt-1 opacity-60 text-[#00FFFF]'>07</p></motion.div>
                     </a>
-                    <a href="https://app.codemate.ai" target="_blank">
-                      <motion.div whileHover={{ opacity: 0.6 }} className='flex gap-2 cursor-pointer'><h1 className='MenuText'>GET STARTED</h1><p className='text-[3vw] mt-1 opacity-60 text-[#00FFFF]'>08</p></motion.div>
-                    </a>
+                    <motion.div onClick={() => { setMenu(false); router.push('/contact'); }} whileHover={{ opacity: 0.6 }} className='flex gap-2 cursor-pointer'><h1 className='MenuText'>CONTACT US</h1><p className='text-[3vw] mt-1 opacity-60 text-[#00FFFF]'>08</p></motion.div>
                   </div>
 
                   <div className='mt-[15rem] w-full bg-zinc-900'>
