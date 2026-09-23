e9df53c703a55521d8e9bf9bb0e367338b6b5a05
BISWAJIT DASH <137093538+Biswajitdash-09@users.noreply.github.com>
2026-09-23 17:55:32 +0530

    Remove geolocation from Permissions-Policy

--- next.config.ts
+++ next.config.ts
@@ -44,7 +44,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
+            value: "camera=(), microphone=()",
           },
           {
             key: "X-DNS-Prefetch-Control",
